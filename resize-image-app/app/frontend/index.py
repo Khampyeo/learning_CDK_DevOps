d0edeb59e6f53b2ec0ca6d6c81925aeb9bbca7c0
kham <ckiet1201@gmail.com>
2023-12-22 11:39:13 +0700

    add new feature resize0image-app

--- resize-image-app/app/frontend/index.py
+++ resize-image-app/app/frontend/index.py
@@ -3,38 +3,66 @@ import base64
 import requests
 import json
 
+
 def main():
-    st.title("Resize Image")
+    st.title("Edit Image")
 
-    col1, col2 = st.columns(2)
-
-    uploaded_file = col1.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
-    with col1:
-        widthInput = st.number_input("Enter width", min_value=10, step=1, value=100)
+    col11, col12 = st.columns(2)
+    col1, col2, col3 = st.columns([2,1,1])
     
-    with col1:
-        heightInput = st.number_input("Enter height", min_value=10, step=1, value=100)
+    with col2:
+        widthInput = st.number_input("Enter Width:", min_value=10, step=1, value=100)
+        grayscale = st.radio("Remove Color:", [True, False], index=1)
+
+        st.write("")
+        btn = st.button("Resize Image", type="primary")
+
+    with col3:
+        heightInput = st.number_input("Enter Height:", min_value=10, step=1, value=100)
+        crop = st.radio("Crop Image", [True, False], index=0)
+
+        
+    with col12:
+        quality = st.slider("Image Quality:", min_value=1, max_value=100, value=100, step=1)
+        type = st.radio("Save Image As:", ["png", "jpg","jpeg","original"], index=3, horizontal=True)
+        
+
+    
+    uploaded_file = col11.file_uploader("Choose a image", type=["jpg", "jpeg", "png"])
+    
+    if uploaded_file : 
+        reset_values()
 
     if uploaded_file is not None:
-        col2.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
-        col2.write("")
         
-        if st.button("Resize Image"):
-            with st.spinner("Resize..."):
+
+        col1.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+        
+        if btn:
+            with st.spinner("Editting..."):
                 url = "https://gy7n5q6gjg.execute-api.ap-southeast-1.amazonaws.com/prod/image"
                 base64_data = base64.b64encode(uploaded_file.read()).decode("utf-8")
-
                 fileName =  uploaded_file.name
-                type= uploaded_file.type
+                if type == "original":
+                    type = uploaded_file.type.split("/")[1]
+                    contentType= uploaded_file.type
+                else:   
+                    contentType = "image/" + type
+
                 params={
                     "width" : widthInput,
-                    "height": heightInput
+                    "height": heightInput,
+                    "grayscale":grayscale,
+                    "quality":quality,
+                    "crop":crop,
+                    "type":type
                 }
                 data = {
                     "name":fileName,
-                    "type":type,
+                    "type":contentType,
                     "file":base64_data
                 }
+
                 response = requests.post(url, params=params, data=json.dumps(data))
             st.write("Status Code:", response.status_code)
             if(response.status_code==200):
--- resize-image-app/backend/src/index.ts
+++ resize-image-app/backend/src/index.ts
@@ -18,26 +18,77 @@ export const handler = async (event) => {
   const width = Number(event.queryStringParameters.width);
   const height = Number(event.queryStringParameters.height);
 
+  const grayscale =
+    event.queryStringParameters.grayscale === "True" ? true : false;
+
+  const type = event.queryStringParameters.type;
+  const crop = event.queryStringParameters.crop === "True" ? true : false;
+  const quality = Number(event.queryStringParameters.quality);
+
   try {
     const client = new S3Client({ region });
 
     const parsedBody = JSON.parse(event.body);
     const base64File = parsedBody.file;
-    const type = parsedBody.type;
-    const name = parsedBody.name;
+    const ContentType = parsedBody.type;
+    let name = parsedBody.name;
 
     const decodedFile = Buffer.from(
       base64File.replace(/^data:image\/\w+;base64,/, ""),
       "base64"
     );
-
-    const image = await sharp(decodedFile).resize(width, height).toBuffer();
+    let result = name.split(".").pop();
+    name = name.replace(`.${result}`, `.${type}`);
+    let image;
+    switch (type) {
+      case "png":
+        image = await sharp(decodedFile)
+          .resize({
+            width: width,
+            height: height,
+            withoutEnlargement: crop,
+            fit: crop ? "cover" : "fill",
+          })
+          .toFormat("png")
+          .png({ quality: quality })
+          .grayscale(grayscale)
+          .toBuffer();
+        break;
+      case "jpeg":
+        image = await sharp(decodedFile)
+          .resize({
+            width: width,
+            height: height,
+            withoutEnlargement: crop,
+            fit: crop ? "cover" : "fill",
+          })
+          .toFormat("jpeg")
+          .jpeg({ quality: quality })
+          .grayscale(grayscale)
+          .toBuffer();
+        break;
+      case "jpg":
+        image = await sharp(decodedFile)
+          .resize({
+            width: width,
+            height: height,
+            withoutEnlargement: crop,
+            fit: crop ? "cover" : "fill",
+          })
+          .toFormat("jpg")
+          .jpeg({ quality: quality })
+          .grayscale(grayscale)
+          .toBuffer();
+        break;
+      default:
+        break;
+    }
 
     const params = {
       Bucket: bucketName,
       Key: name || "null.png",
       Body: image,
-      ContentType: type || "image/png",
+      ContentType: ContentType || "image/png",
     };
 
     const command = new PutObjectCommand(params);
