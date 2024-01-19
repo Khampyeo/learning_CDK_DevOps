c9172055bf3dacd39ae317342e9819a8d982898e
kham <ckiet1201@gmail.com>
2024-01-19 08:53:57 +0700

    a

--- resize-image-app/app/frontend/index.py
+++ resize-image-app/app/frontend/index.py
@@ -6,6 +6,7 @@ from streamlit_option_menu import option_menu
 import pandas as pd
 from streamlit.components.v1 import html
 
+
 st.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>', unsafe_allow_html=True)
 
 def downloadImage(key):
